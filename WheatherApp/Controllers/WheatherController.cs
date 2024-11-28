61aa0f08f50a28e60304ee51bc44df5546a34b37
Ayman Hussain <139609266+ay77man@users.noreply.github.com>
2024-11-28 22:19:50 +0200

    Add project files.

---
+++ WheatherApp/Controllers/WheatherController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
+using WheatherApp.Models;
+
+namespace WheatherApp.Controllers
+{
+    public class WheatherController : Controller
+    {
+        List<CityWeather> cityWeatherList = new List<CityWeather>
+        {
+            new CityWeather{CityUniqueCode = "LDN", CityName = "London", DateAndTime = DateTime.Parse("2030-01-01 8:00"),  TemperatureFahrenheit = 33},
+            new CityWeather{CityUniqueCode = "NYC", CityName = "London", DateAndTime = DateTime.Parse("2030-01-01 3:00"),  TemperatureFahrenheit = 60},
+            new CityWeather{CityUniqueCode = "PAR", CityName = "Paris", DateAndTime = DateTime.Parse("2030-01-01 9:00"),  TemperatureFahrenheit = 82}
+        };
+
+        [Route("/")]
+        [Route("Home")]
+        public IActionResult Home()
+        {
+            return View(cityWeatherList);
+        }
+        [Route("Wheather/{CityUniqueCode}")]
+        public IActionResult Details(string? CityUniqueCode)
+        {
+            if(CityUniqueCode == null)
+            {
+                return Content("InValid CityUniqueCode");
+            }
+            CityWeather? MatchedCityWeather = cityWeatherList.Where(temp=>temp.CityUniqueCode== CityUniqueCode).FirstOrDefault();
+            return View(MatchedCityWeather);
+        }
+    }
+}
